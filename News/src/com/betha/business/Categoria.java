b4869ab9ee3e2ab1ccfd3544b572e4add980f8eb
jaaziel.lopes <jaaziel.lopes@10.27.0.6>
2013-09-03 11:03:40 -0300

    Criado o pacote util com 3 classes

--- News/src/com/betha/business/Categoria.java
+++ News/src/com/betha/business/Categoria.java
@@ -3,10 +3,52 @@
  */
 package com.betha.business;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * @author Lucas Natal
  *
  */
+@Entity
+@Table(name="categorias")
 public class Categoria {
+private Integer id;
+private String nome;
+private Boolean status;
+
+public Categoria(Integer id, String nome, Boolean status) {
+	this.id = id;
+	this.nome = nome;
+	this.status = status;
+}
+
+@Id
+@GeneratedValue
+public Integer getId() {
+	return id;
+}
+
+public void setId(Integer id) {
+	this.id = id;
+}
+
+public String getNome() {
+	return nome;
+}
+
+public void setNome(String nome) {
+	this.nome = nome;
+}
+
+public Boolean getStatus() {
+	return status;
+}
+
+public void setStatus(Boolean status) {
+	this.status = status;
+}
 
 }
