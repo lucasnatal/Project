6b76b2ee5e20df4b365a127be82040679ca851d7
Lucas Natal <bonettinatal@gmail.com>
2013-09-09 09:01:25 -0300

    Alterado o bean notícia e criado a tela de cadastro de notícia.

--- News/src/com/betha/beans/NoticiaMB.java
+++ News/src/com/betha/beans/NoticiaMB.java
@@ -2,19 +2,28 @@ package com.betha.beans;
 
 import java.util.List;
 
+import com.betha.business.Categoria;
 import com.betha.business.Noticia;
+import com.betha.repository.Categorias;
 import com.betha.repository.Noticias;
 import com.betha.util.Repository;
 
 
 public class NoticiaMB {
 	private Noticias noticias;
+	private Categorias categorias;
+
 	private List<Noticia> todasNoticias;
 	private List<Noticia> noticiasExibidas;
+	private List<Categoria> listaCategorias;
 
+	
 	public NoticiaMB() {
 		this.noticias = (Noticias) Repository.getNoticias();
+		this.categorias = (Categorias) Repository.getCategorias();
+		
 		this.todasNoticias = this.noticias.selecionar();
+		this.listaCategorias = this.categorias.selecionar();
 	}
 
 	public List<Noticia> getTodasNoticias() {
